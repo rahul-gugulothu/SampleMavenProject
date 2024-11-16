e3226e079689fddb42c3e62f6d8a38258549f6c8
rahul-gugulothu <rahulg.321r@gmail.com>
2024-11-16 15:18:33 +0530

    committing

---
+++ src/main/java/com/app/demo/SampleMavenJavaProject/App.java
@@ -0,0 +1,14 @@
+package com.app.demo.SampleMavenJavaProject;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        System.out.println("Hello this is Rahul 662N doing Maven java project");
+    }
+}
